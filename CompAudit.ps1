ff0b1233fc569663165604a7dc4d3886920b920b
xandora <mfra109@gmail.com>
2017-08-24 11:18:33 +1200

    Add files via upload

---
+++ CompAudit.ps1
@@ -0,0 +1,35 @@
+﻿$begin = get-date
+$CompAudit = Import-Csv C:\Powershell\CompAudit\CompAudit.csv
+#CompAudit.csv is a CSV file with IP,Computer fields
+$Date = Get-Date -Format yyyyMMddhhmm
+$i = 0
+
+$output = foreach ($Comp in $CompAudit) {
+
+    $IP = $Comp.IPAddress
+    $result = Test-Connection $IP -Count 1 -Quiet -ErrorAction SilentlyContinue
+
+    if (!$result) {
+        
+        $Comp.Computer = Write-Output "$IP not valid, or not online."
+
+    } else {
+        
+        Try {
+            $Comp.Computer = Write-Output ([System.Net.Dns]::GetHostbyAddress($IP)).HostName #| Select-Object @{Name='HostName';Expression={$_}} #| Export-Csv C:\Powershell\CompAudit\CompAudit-Output-$Date.csv -NoClobber -Append -NoTypeInformation
+        } 
+        Catch {
+            $Comp.Computer = Write-Output "$IP is reachable, but not resolvable." #| Select-Object @{Name='HostName';Expression={$_}} #| Export-Csv C:\Powershell\CompAudit\CompAudit-Output-$Date.csv -Append -NoTypeInformation
+        }
+    }
+    $Comp
+    $i++
+    Write-Progress -activity "Resolving IPs . . ." -status "Resolved: $i of $($CompAudit.Count)" -percentComplete (($i / $CompAudit.Count)  * 100)
+}
+
+$output | Out-GridView
+$output | Export-Csv C:\Powershell\CompAudit\output\CompAudit-Output-$Date.csv -NoTypeInformation
+$finish = get-date
+($finish - $begin).ToString()
+
+
---
+++ Get-MFADGroup.ps1
@@ -0,0 +1,75 @@
+﻿<#
+.Synopsis
+  Get-MFADGroup
+.DESCRIPTION
+   This command generates detailed reports on AD Groups. Allows for looping through nested groups.
+.EXAMPLE
+  Get-MFADGroup -Group AD-Sec-Group
+
+.EXAMPLE
+  Get-MFADGroup -Group AD-Sec-Group -Recurse
+
+.EXAMPLE
+  Get-MFADGroup -Group AD-Sec-Group -Recurse -Export C:\ExampleOutput
+
+#>
+functionGet-MFADGroup{
+    [CmdletBinding()]
+    Param(
+        # Target AD Group
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        $Group,
+
+        # Loop through any nested Groups
+        [Switch]
+        $Recurse,
+
+        # Path to export CSV files to
+        [String]
+        $Export
+        )
+
+    $target = Get-ADGroupMember $Group
+
+    if ($Export){
+            Try {
+                $exportName = (Get-ADGroup -Filter {DisplayName -like $Group} -Properties DisplayName -ErrorAction Inquire).DisplayName
+                Write-Output "(Try) Group Name: $Group"
+                Write-Output "(Try) Export name: $exportName"
+            } Catch {
+                $exportName = (Get-ADGroup $Group -Properties DisplayName | Select DisplayName).DisplayName
+            }
+            foreach($t in $target) {
+            if ($t.objectClass -eq "user") {
+                $users = Get-ADUser $t.name -Properties GivenName, Surname, Name, EmailAddress, Enabled | select GivenName, Surname, Name, EmailAddress, Enabled
+                $users | Export-Csv $Export\$ExportName.csv -Append -NoTypeInformation
+            } elseif ($t.objectClass -eq "group") {
+                if ($Recurse) {
+                   Get-MFADGroup $t.SamAccountName -Recurse -Export $Export
+                } else {
+                    Get-ADGroup $t.SamAccountName -Properties DisplayName, Description | select DisplayName, Description
+                }
+            }
+        }
+
+    } else {
+
+            foreach($t in $target) {
+                if ($t.objectClass -eq "user") {
+                    Get-ADUser $t.name -Properties GivenName, Surname, Name, EmailAddress, Enabled | select GivenName, Surname, Name, EmailAddress, Enabled
+                } elseif ($t.objectClass -eq "group") {
+                    if ($Recurse) {
+                       Get-MFADGroup $t.SamAccountName -Recurse
+                    } else {
+                        Get-ADGroup $t.SamAccountName -Properties DisplayName, Description | select DisplayName, Description
+                    }
+                }
+            }
+        }
+}
+
+#$Group = "AUK_All_Users - Consumer"
+#$g = Get-ADGroup $Group -Properties *
+#$g.Members | get-adgroup
